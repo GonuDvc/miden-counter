c9b17292c3453e7ce2fc326815903794b463604b
Dvc <jevejdvc@gmail.com>
2026-05-17 11:20:24 +0200

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub struct Counter {
+    value: i32,
+}
+
+impl Counter {
+    pub fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    pub fn increment(&mut self) {
+        self.value += 1;
+    }
+
+    pub fn decrement(&mut self) {
+        self.value -= 1;
+    }
+
+    pub fn get(&self) -> i32 {
+        self.value
+    }
+}
